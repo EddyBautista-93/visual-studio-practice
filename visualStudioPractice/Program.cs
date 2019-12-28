36100740719072f8aea05546aec191b6cc7104ba
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-12-28 13:50:12 -0600

    Initial check-in of module visualStudioPractice

---
+++ visualStudioPractice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace visualStudioPractice
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+      
+                //Creating a new random number Generator
+                System.Random generator = new System.Random();
+                //generate a floating point number between 0 and 1
+                double randomNumber = generator.NextDouble();
+                //Round a number to a single decimal place
+                double roundedNumber = System.Math.Round(randomNumber, 1);
+                //pring out the random number
+                System.Console.WriteLine(roundedNumber);
+        }
+    }
+}
