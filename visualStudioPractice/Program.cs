26a928b14e772b53cd1e2588b3838be7fcd9363a
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-12-30 14:32:08 -0600

    Added some comments and practicing pushing up to a repo on github

--- visualStudioPractice/Program.cs
+++ visualStudioPractice/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//with useing we are save time from having to type the namespace all the time
+using System;
 
 namespace visualStudioPractice
 {
@@ -8,13 +9,13 @@ namespace visualStudioPractice
         {
       
                 //Creating a new random number Generator
-                System.Random generator = new System.Random();
+                Random generator = new Random();
                 //generate a floating point number between 0 and 1
                 double randomNumber = generator.NextDouble();
                 //Round a number to a single decimal place
-                double roundedNumber = System.Math.Round(randomNumber, 1);
+                double roundedNumber = Math.Round(randomNumber, 1);
                 //pring out the random number
-                System.Console.WriteLine(roundedNumber);
+                Console.WriteLine(roundedNumber);
         }
     }
 }
