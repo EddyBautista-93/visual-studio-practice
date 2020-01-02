2f519ea0f03db809eb8c8923120fa057984bf9e9
Eddy Bautista <bautista.j.eddy@gmail.com>
2020-01-01 20:31:15 -0600

    Bulding off the treehouse project, going over functions like java.

--- visualStudioPractice/Program.cs
+++ visualStudioPractice/Program.cs
@@ -1,21 +1,29 @@
 ﻿//with useing we are save time from having to type the namespace all the time
 using System;
+using System.Threading;
 
-namespace visualStudioPractice
-{
+
     class MainClass
     {
+
+    static void Wait()
+    {
+        Console.WriteLine("Welcome to the cat food store");
+        Thread.Sleep(3000);
+        Console.WriteLine("What can i do for you?");
+    }
+
+    static void CountToThree()
+    {
+        Console.WriteLine(1);
+        Console.WriteLine(2);
+        Console.WriteLine(3);
+    }
+
         public static void Main(string[] args)
         {
-      
-                //Creating a new random number Generator
-                Random generator = new Random();
-                //generate a floating point number between 0 and 1
-                double randomNumber = generator.NextDouble();
-                //Round a number to a single decimal place
-                double roundedNumber = Math.Round(randomNumber, 1);
-                //pring out the random number
-                Console.WriteLine(roundedNumber);
+        Wait();
+        CountToThree();
         }
-    }
-}
+     }
+
